40f0d9e01f593f54d7967e919d1b1c30b120905d
sciphi-d <me@example.com>
2025-04-05 19:57:10 +0330

    initial implementation of search, added to run function

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,23 @@ pub mod config;
 pub fn run(args: Vec<String>) -> Result<(), Box<dyn Error>> {
     let config = config::Config::new(&args)?;
     let content = fs::read_to_string(&config.file_path)?;
-    println!("Searching for `{}` in `{}`",config.query, config.file_path);
-    println!("File content:\n{content}");
+    // println!("Searching for `{}` in `{}`",config.query, config.file_path);
+    // println!("File content:\n{content}");
+    let result = search(&config.query, &content);
+    for m in result {
+        println!("{m}");
+    }
     Ok(())
 }
 
-pub fn search<'a>( _query: &str, _contents: &'a str) -> Vec<&'a str> {
-    vec![]
+pub fn search<'a>( query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result: Vec<&str> = vec![];
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line.trim());
+        }
+    }
+    result
 }
 
 #[cfg(test)]
